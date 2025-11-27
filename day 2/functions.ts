23601adfaaa8905aa1ecdae52a57b822f2174bfb
Hemnath M <146712643+HEMNATH77@users.noreply.github.com>
2025-11-27 18:55:31 +0530

    Add files via upload

---
+++ day 2/functions.ts
@@ -0,0 +1,40 @@
+// Functions - > ensures  1. Proper parameter   2. Proper return type   3. Catch errors before execution
+
+// 1. By Parameter type + Return Type
+
+// function hello(name : string): string {
+//     return "Welcome " + name;
+// }
+
+// console.log(hello("Zara"))    // op : Welcome Zara
+
+// 2. By Default Parameters
+
+// function score(name:string, score : number){
+//     console.log( name + " : " + score )
+// }
+// score("hem",97)            // op : hem : 97
+
+// 3. By optional parameters (?) -> not compulsory to pass parameters
+
+function my_name(name : string,initial? : string){
+    console.log(name,initial)
+}
+
+my_name("Gio");        // Gio  undefined
+
+
+// 4. By Rest parameters(...)  -> used when no.of parameters are unknown 
+
+function total(...a:number[]){
+    return a;
+}
+
+console.log(total(10,20))          // op : [10,20]
+ 
+
+
+
+
+
+
